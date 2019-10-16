71f4c181fcf58633169eb831dd4c2c3ee0b28828
JingMingChan <prochan75103161@gmail.com>
2019-10-17 00:24:15 +0800

    DiceRollerVer3

--- app/src/main/java/com/example/pratical1wed2/MainActivity.kt
+++ app/src/main/java/com/example/pratical1wed2/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.pratical1wed2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import java.util.Random;
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +18,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice(){
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "button clicked",
+        //    Toast.LENGTH_SHORT).show()
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
+        val randomInt1 = Random().nextInt(6) + 1
+        val randomInt2 = Random().nextInt(6) + 1
+        val randomInt3 = Random().nextInt(6) + 1
+        resultText1.text = randomInt1.toString()
+        resultText2.text = randomInt2.toString()
+        resultText3.text = randomInt3.toString()
     }
+
+
 }
