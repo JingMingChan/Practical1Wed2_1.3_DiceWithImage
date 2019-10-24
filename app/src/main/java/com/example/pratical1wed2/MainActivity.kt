993e65bae2f6e09969b61b406d945a50876d5ff5
JingMingChan <prochan75103161@gmail.com>
2019-10-24 19:26:16 +0800

    dice roller with image ver 2

--- app/src/main/java/com/example/pratical1wed2/MainActivity.kt
+++ app/src/main/java/com/example/pratical1wed2/MainActivity.kt
@@ -3,17 +3,24 @@ package com.example.pratical1wed2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.Random;
 import android.widget.Toast
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+    lateinit var diceImage3 : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage1 = findViewById(R.id.dice1)
+        diceImage2 = findViewById(R.id.dice2)
+        diceImage3 = findViewById(R.id.dice3)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
@@ -23,13 +30,26 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Dice Rolled",
            Toast.LENGTH_SHORT).show()
 
-
         val randomInt1 = Random().nextInt(6) + 1
         val randomInt2 = Random().nextInt(6) + 1
         val randomInt3 = Random().nextInt(6) + 1
+
+        diceImage1.setImageResource(DiceNum(randomInt1))
+        diceImage2.setImageResource(DiceNum(randomInt2))
+        diceImage3.setImageResource(DiceNum(randomInt3))
     }
 
-
+    private fun DiceNum(randNum: Int):Int{
+        val drawableResource = when (randNum) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableResource
+    }
 
 
 }
